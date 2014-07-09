3dd4335e6e1d2aad81af26212ebaa32e587a1d75
Amber Moss <amberdmoss@gmail.com>
2014-07-09 14:17:28 -0600

    finished array and quiz js

--- arrayProperty.js
+++ arrayProperty.js
@@ -1,5 +1,12 @@
 //Just like you can add methods to your own constructor, you can also add methods and properties to built in classes in JavaScript like Arrays and Objects.
 
 //Add a reverse method to the String 'class' so that every instance of String can call reverse and reverse itself.
+
+
+	function reverse(s) {
+  return s.split('class').reverse().join('');
+}
+
+
   
   //code here
